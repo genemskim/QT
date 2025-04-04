c304be35e57b8cee73a04c209ebf5606af30ca12
MinSuk KIM <ms.e.kim@gmail.com>
2025-04-04 20:29:58 +0900

    add error handler

--- down.py
+++ down.py
@@ -3,7 +3,7 @@ import datetime
 from urllib import request
 
 if len(sys.argv) > 1:
-    date = datetime.datetime.strptime(sys.argv[1], "%Y%m%d") 
+    date = datetime.datetime.strptime(sys.argv[1], "%Y%m%d")
     year = date.year
     day = date.__format__("%Y%m%d")
 else :
@@ -17,11 +17,15 @@ print(day)
 
 nextDay = datetime.timedelta(days=1)
 
-while 1 :
-    url = "http://meditation.su.or.kr/meditation_mp3/%d/%s.mp3" %(year,day)
-    localFile = "%s.mp3" %day
+while True :
+    try :
+        url = "http://meditation.su.or.kr/meditation_mp3/%d/%s.mp3" %(year,day)
+        localFile = "%s.mp3" %day
+        print(url)
+        request.urlretrieve(url, localFile)
+    except Exception as e :
+        print("Error: %s" %e)
+        break
     date += nextDay
     year = date.year
     day = date.__format__("%Y%m%d")
-    print(url)
-    request.urlretrieve(url, localFile)
